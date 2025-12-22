0fa01a4b4bee54285588ece262b92f33f4228bcb
Igor Manushin <igor.manushin@pm.me>
2025-12-22 19:07:33 +0000

    initial commit

---
+++ artifactory-publish/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("io.github.build-extensions-oss.helm") version "3.0.0"
+    id("io.github.build-extensions-oss.helm-publish") version "3.0.0"
+}
+
+helm {
+    charts {
+        create("main") {
+            // this is default option - however let's have it explicit
+            publish = true
+        }
+    }
+    publishing {
+        repositories {
+            artifactory {
+                url = uri("http://localhost:8080/artifactory/folder1/folder2")
+
+                credentials {
+                    username = "testUserName"
+                    password = "testPassword"
+                }
+            }
+        }
+    }
+}
